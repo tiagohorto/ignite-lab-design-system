3de9c6370b089026e2921a5f6eeee011778afa02
TiagoHorto <tiagohorto.07@gmail.com>
2022-10-13 11:25:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <h1 className='text-9xl'>Hello world!!</h1>
+  )
+}
+
---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,40 @@
+import { Text, TextProps } from "./Text";
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem Impsum.'
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Text with p</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}
